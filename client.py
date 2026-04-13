12a99db20c49459e2b8234ec3ebc72924378d7a4
Stealth-squirrel <nkudr.1823@gmail.com>
2026-04-13 14:54:52 +0300

    part 2

--- client.py
+++ client.py
@@ -1,46 +1,28 @@
 import socket
-
 import random
+from utils import xor_encrypt, xor_decrypt
 
 
 HOST = '127.0.0.1'
-
-PORT = 5000
+PORT = 5001
 
 
 p = 23
-
 g = 5
-
 
 client_secret = random.randint(1, 100)
 
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
     s.connect((HOST, PORT))
 
-
-    # считаем A
-
     A = pow(g, client_secret, p)
-
-
-    # отправляем A
-
     s.send(str(A).encode())
 
-
-    # получаем B
-
-    data = s.recv(1024).decode()
-
-    B = int(data)
-
-
-    # общий ключ
-
+    B = int(s.recv(1024).decode())
     K = pow(B, client_secret, p)
 
+    message = "Hello server"
+    s.send(xor_encrypt(message, K))
 
-    print(f"[CLIENT] Shared key: {K}")
+    response = xor_decrypt(s.recv(1024), K)
+    print(f"[CLIENT] Response: {response}")
