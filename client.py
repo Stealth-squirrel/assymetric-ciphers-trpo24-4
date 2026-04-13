ce23a8f07640479f91b0575973a9892925dbefc2
Stealth-squirrel <nkudr.1823@gmail.com>
2026-04-13 14:51:01 +0300

    part 1

--- client.py
+++ client.py
@@ -1,14 +1,46 @@
 import socket
-import pickle
+
+import random
+
 
 HOST = '127.0.0.1'
-PORT = 8080
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
+PORT = 5000
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
 
-sock.close()
+p = 23
+
+g = 5
+
+
+client_secret = random.randint(1, 100)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    s.connect((HOST, PORT))
+
+
+    # считаем A
+
+    A = pow(g, client_secret, p)
+
+
+    # отправляем A
+
+    s.send(str(A).encode())
+
+
+    # получаем B
+
+    data = s.recv(1024).decode()
+
+    B = int(data)
+
+
+    # общий ключ
+
+    K = pow(B, client_secret, p)
+
+
+    print(f"[CLIENT] Shared key: {K}")
